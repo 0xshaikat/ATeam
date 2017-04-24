d9c888202c5f7d9dd7ad040b53ea7ff64973ac48
Shaikat Islam <sislam9@stuy.edu>
2017-04-24 01:47:01 -0400

    Update HelpDesk.java

--- HelpDesk.java
+++ HelpDesk.java
@@ -15,32 +15,53 @@ public class HelpDesk{
     ticketprocessed = 0;
   }
 
-
+  //create a ticket
   public static void create(){
     //initialize ticket;
     Ticket tick = new Ticket();
     //initialize identifier token
-    Ticket.setID(ticketprocessed);
+    tick.setID(ticketprocessed);
     //ensure next ticket has a higher number
     ticketprocessed ++;
     //name of the dude/dudette
     System.out.println("What is your name, sir?");
     String name = Keyboard.readString();
-    Ticket.setName(name);
+    tick.setName(name);
     //what is the problem?
-    System.out.println("For the clarity of our services, we offer help for three problems, namely those dealing with money, time, and space. What is the nature of your problem?");
+    System.out.println("What is the nature of your problem?");
     String problem = Keyboard.readString();
+    tick.setProb(problem);
     // VIP Level
     System.out.println("On a scale from 0 to 5, where 0 means I must do your bidding or else I lose my job, and 5 means you're an intern who I could care less about, please determine your importance at FMS Corp.");
     int viplvl = Keyboard.readInt();
+    tick.setVIP(viplvl);
     System.out.println("Thank you for using FMS Corp. Your services are appreciated.");
+    System.out.println("Here is your ticket, for your own purposes: " + tick);
     customers.add(tick);
+    System.out.println("If you have more tickets, entere 'true' in the next dialogue.");
+    Boolean bool = Keyboard.readBoolean();
+    if(bool){
+      create();
+    }
+  }
+
+ //fix their problems in order of priority (vip level)
+  public static void solve(){
+    while(!(customers.isEmpty())){    Ticket solved = customers.removeMin();
+        solved.setSolved(true);
+        solved.setSolution("Turned it off and on again.");
+        System.out.println("This ticket was solved: " + solved);
+      }
   }
 
   public static void main (String[] args){
     HelpDesk help = new HelpDesk();
+    //there is a problem where once you add a bunch of tickets, all that registers within APQ customers is the latest ticket being added.
+    //would be nice if there was a fix.
     create();
-    System.out.println(customers);
+    solve();
+    System.out.println("APQ of customers: " + customers);
+
   }//end main
 
 }//end class
